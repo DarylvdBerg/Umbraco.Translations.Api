ba209e870c8a919324f6d34f4b63f6f744567ec8
Daryl van den Berg <darylvdberg@gmail.com>
2025-01-18 15:30:26 +0100

    Have a single method for registring all dependencies

--- Umbraco.Translations.Api/Extensions/ServiceCollectionExtensions.cs
+++ Umbraco.Translations.Api/Extensions/ServiceCollectionExtensions.cs
@@ -5,12 +5,30 @@ namespace Umbraco.Translations.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void RegisterPackageDependencies(this IServiceCollection services)
+    /// <summary>
+    /// Register all dependencies and services for the Translation API package.
+    /// </summary>
+    /// <param name="services"></param>
+    public static void RegisterTranslationsApi(this IServiceCollection services)
+    {
+        services.RegisterPackageDependencies();
+        services.RegisterPackageServices();
+    }
+    
+    /// <summary>
+    /// Register third party dependencies.
+    /// </summary>
+    /// <param name="services"></param>
+    private static void RegisterPackageDependencies(this IServiceCollection services)
     {
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());   
     }
 
-    public static void RegisterPackageServices(this IServiceCollection services)
+    /// <summary>
+    /// Register project services
+    /// </summary>
+    /// <param name="services"></param>
+    private static void RegisterPackageServices(this IServiceCollection services)
     {
         services.AddTransient<ITranslationService, TranslationService>();
     }
