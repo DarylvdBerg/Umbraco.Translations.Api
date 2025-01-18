a1fcf154a63e55f27e1a6f2d65b1933d4c91ed20
Daryl van den Berg <darylvdberg@gmail.com>
2025-01-18 17:58:25 +0100

    Add temp translation and ensure collection gets mapped

--- Umbraco.Translations.Api/Mappers/TranslationMapper.cs
+++ Umbraco.Translations.Api/Mappers/TranslationMapper.cs
@@ -4,15 +4,27 @@ using Umbraco.Translations.Api.Models;
 
 namespace Umbraco.Translations.Api.Mappers;
 
+/// <summary>
+/// Mapping profile for converting IDictionaryTranslation to ITranslation
+/// </summary>
 public class TranslationMapper : Profile
 {
     public TranslationMapper()
     {
         CreateMap<IDictionaryTranslation, Translation>()
-            .ForMember(src => src.Key, opt => opt.MapFrom(src => src.Key))
+            .ForMember(src => src.Id, opt => opt.MapFrom(src => src.Key))
+            .ForMember(src => src.Key, opt => opt.MapFrom(src => ""))
             .ForMember(src => src.Value, opt => opt.MapFrom(src => src.Value))
             .ForMember(src => src.Culture, opt => opt.MapFrom(src => src.LanguageIsoCode));
 
         CreateMap<IDictionaryTranslation, ITranslation>().As<Translation>();
+        
+        // Map IEnumerable<Source> to IList<Target>
+        CreateMap<IEnumerable<IDictionaryTranslation>, IList<Translation>>()
+            .ConvertUsing((src, dest, context) =>
+                src.Select(item => context.Mapper.Map<Translation>(item)).ToList());
+        CreateMap<IEnumerable<IDictionaryTranslation>, IList<ITranslation>>()
+            .ConvertUsing((src, dest, context) =>
+                src.Select(item => context.Mapper.Map<ITranslation>(item)).ToList());
     }
 }
