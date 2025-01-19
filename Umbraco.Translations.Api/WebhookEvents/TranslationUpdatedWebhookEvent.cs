e6e920a976671cc5ddd4f9b43fa7b06ee73f345b
Daryl van den Berg <darylvdberg@gmail.com>
2025-01-19 14:13:24 +0100

    Added basics for implementing a webhook event in umbraco

---
+++ Umbraco.Translations.Api/WebhookEvents/TranslationUpdatedWebhookEvent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Sync;
+using Umbraco.Cms.Core.Webhooks;
+
+namespace Umbraco.Translations.Api.NotificationHandlers;
+
+public class TranslationUpdatedWebhookEvent : WebhookEventContentBase<ContentPublishedNotification, IContent>
+{
+    public TranslationUpdatedWebhookEvent(IWebhookFiringService webhookFiringService, IWebhookService webhookService, IOptionsMonitor<WebhookSettings> webhookSettings, IServerRoleAccessor serverRoleAccessor) : base(webhookFiringService, webhookService, webhookSettings, serverRoleAccessor)
+    {
+    }
+
+    public override string Alias { get; }
+    protected override IEnumerable<IContent> GetEntitiesFromNotification(ContentPublishedNotification notification)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override object? ConvertEntityToRequestPayload(IContent entity)
+    {
+        throw new NotImplementedException();
+    }
+}
