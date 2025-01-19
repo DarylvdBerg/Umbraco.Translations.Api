64e931290732625249a77e77154f8948a976035e
Daryl van den Berg <darylvdberg@gmail.com>
2025-01-19 14:37:00 +0100

    Implemented a notification handler that will remove all instances of updated entry from the cache if updated.

---
+++ Umbraco.Translations.Api/Composer/UmbracoTranslationApiComposer.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Translations.Api.NotificationHandlers;
+
+namespace Umbraco.Translations.Api.Composer;
+
+public class UmbracoTranslationApiComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.AddNotificationHandler<DictionaryItemSavedNotification, TranslationUpdatedNotificationHandler>();
+    }
+}
---
+++ Umbraco.Translations.Api/NotificationHandlers/TranslationUpdatedNotificationHandler.cs
@@ -0,0 +1,41 @@
+﻿using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Translations.Api.Models;
+using Umbraco.Translations.Api.Services;
+using Umbraco.Translations.Api.Strategy;
+
+namespace Umbraco.Translations.Api.NotificationHandlers;
+
+public class TranslationUpdatedNotificationHandler: INotificationHandler<DictionaryItemSavedNotification>
+{
+    private readonly ICacheStrategy<ITranslation> _cacheStrategy;
+    private readonly ICacheKeyBuilder _cacheKeyBuilder;
+
+    public TranslationUpdatedNotificationHandler(ICacheStrategy<ITranslation> cacheStrategy, ICacheKeyBuilder cacheKeyBuilder)
+    {
+        _cacheStrategy = cacheStrategy;
+        _cacheKeyBuilder = cacheKeyBuilder;
+    }
+    
+    public void Handle(DictionaryItemSavedNotification notification)
+    {
+        // Get all saved entities
+        var entities = notification.SavedEntities;
+        
+        // Loop over each entity to remove from cache.
+        foreach (var entity in entities)
+        {
+            var dictionaryKey = entity.ItemKey;
+            var cultures = entity.Translations.Select(trans => trans.LanguageIsoCode);
+            
+            // Loop over each configured culture to remove.
+            foreach (var culture in cultures)
+            {
+                var cacheKey = _cacheKeyBuilder.BuildCacheKey([dictionaryKey, culture]);
+                _cacheStrategy.RemoveFromCache(cacheKey);
+            }
+        }
+    }
+}
