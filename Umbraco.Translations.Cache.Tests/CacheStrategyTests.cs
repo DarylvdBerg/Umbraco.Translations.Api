a7d9300434e92b60bf90524df774a54eea85ac9a
DarylvdBerg <darylvdberg@gmail.com>
2025-06-13 15:59:54 +0200

    Add additional cache test

--- Umbraco.Translations.Cache.Tests/CacheStrategyTests.cs
+++ Umbraco.Translations.Cache.Tests/CacheStrategyTests.cs
@@ -47,4 +47,31 @@ public class CacheStrategyTests
         Assert.That(result.Value, Is.EqualTo(cachedItem.Value));
         _cacheProvider.Verify(x => x.AddToCache(It.IsAny<string>(), It.IsAny<Translation>()), Times.Never);
     }
+
+    [Test]
+    public void Should_Return_Item_From_Fallback_Func_If_Item_Is_Not_In_Cache()
+    {
+        // Arrange
+        var cachedItem = new Translation()
+        {
+            Culture = "en-US",
+            Id = Guid.NewGuid(),
+            Value = "Hello World"
+        };
+        
+        var mockFunc = new Mock<Func<Translation>>();
+        mockFunc.Setup(x => x())
+            .Returns(cachedItem);
+        _cacheProvider.Setup(x => x.FetchThroughCache<ITranslation>(It.IsAny<string>()))
+            .Returns<ITranslation>(null!);
+
+        // Act
+        var result = _cacheImplementation.FetchSingleCachedItem<ITranslation>(["key"], mockFunc.Object);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.That(result.Culture, Is.EqualTo(cachedItem.Culture));
+        Assert.That(result.Value, Is.EqualTo(cachedItem.Value));
+        mockFunc.Verify(x => x(), Times.Once);
+    }
 }
