835f18d9f601618142ce122fc0bbadc621df8421
DarylvdBerg <darylvdberg@gmail.com>
2025-06-13 14:43:28 +0200

    Add service tests

---
+++ Umbraco.Translations.Services.Tests/TranslationServiceTests.cs
@@ -0,0 +1,62 @@
+using Moq;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Translations.Services.Models;
+
+namespace Umbraco.Translations.Services.Tests;
+
+public class TranslationServiceTests
+{
+    private Mock<IUmbracoLocalizationWrapperService> _mockWrapperLocalization;
+    private ITranslationService _service;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _mockWrapperLocalization = new Mock<IUmbracoLocalizationWrapperService>();
+        _service = new TranslationService(_mockWrapperLocalization.Object);
+    }
+
+    [Test]
+    public void Should_Return_Null_When_Translation_Not_Found()
+    {
+        // Arrange
+        _mockWrapperLocalization.Setup(x => x.GetDictionaryTranslation(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns<IDictionaryTranslation>(null);
+        
+        // Act
+        var translation = _service.GetTranslationByCulture("en-US", "non.existent.key");
+        
+        // Assert
+        Assert.IsNull(translation);
+    }
+
+    [Test]
+    public void Should_Return_Null_When_Translation_Value_Is_NullOrEmpty()
+    {
+        // Arrange
+        _mockWrapperLocalization.Setup(x => x.GetDictionaryTranslation(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(new DictionaryTranslation(new Language("en-US", "English"), ""));
+        
+        // Act
+        var translation = _service.GetTranslationByCulture("en-US", "non.existent.key");
+        
+        // Assert
+        Assert.IsNull(translation);
+    }
+    
+    [Test]
+    public void Should_Return_Translation_When_Found()
+    {
+        // Arrange
+        var expectedTranslation = new DictionaryTranslation(new Language("en-US", "English"), "Hello World");
+        _mockWrapperLocalization.Setup(x => x.GetDictionaryTranslation(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(expectedTranslation);
+        
+        // Act
+        var translation = _service.GetTranslationByCulture("en-US", "greeting.hello");
+        
+        // Assert
+        Assert.IsNotNull(translation);
+        Assert.That(expectedTranslation.Value, Is.EqualTo("Hello World"));
+    }
+}
